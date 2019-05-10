b4ba13404633c92f7cc9b25c0980873419d309cb
Steven Whitton <stevenawhitton@gmail.com>
2019-05-10 12:07:13 +0200

    Made an attempt to use API call for data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,86 @@
 import React from "react";
 import Header from "./comps/header";
 import Main from "./comps/main";
+const axios = require("axios");
+const header = {
+  headers: {
+    authorization:
+      "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6IjRlNWQwZWJmLWE2ZjUtNDgzYi1iNjVlLWEyZDdjZDMzYWQ2OSIsImlzX3N1YnN5c3RlbSI6InRydWUiLCJwZXJtaXNzaW9uX2lkIjoiOSIsImlhdCI6MTU1NzQ3NzgwMSwiZXhwIjoxNTg5MDEzODAxfQ.tuz5MezdzvscD1IjPORcWgaPfaHZa5tE5wONumptUOI"
+  }
+};
+const newData = [];
+const fakedata = [
+  {
+    logid: 144,
+    title: "faucibus orci luctus",
+    subtype: "comment",
+    origin: "human",
+    creationTime: "22:24:20 30/07/2019",
+    author: 12
+  },
+  {
+    logid: 662,
+    title: "faucibus orci luctus",
+    subtype: "comment",
+    origin: "human",
+    creationTime: "22:24:20 30/07/2019",
+    author: 6
+  },
+  {
+    logid: 976,
+    title: "faucibus orci luctus",
+    subtype: "run",
+    origin: "process",
+    creationTime: "22:24:20 30/07/2019",
+    author: 1
+  },
+  {
+    logid: 976,
+    title: "faucibus orci luctus",
+    subtype: "run",
+    origin: "process",
+    creationTime: "22:24:20 30/07/2019",
+    author: 1
+  },
+  {
+    logid: 976,
+    title: "faucibus orci luctus",
+    subtype: "run",
+    origin: "process",
+    creationTime: "22:24:20 30/07/2019",
+    author: 1
+  },
+  {
+    logid: 976,
+    title: "faucibus orci luctus",
+    subtype: "run",
+    origin: "process",
+    creationTime: "22:24:20 30/07/2019",
+    author: 1
+  }
+];
+
+axios
+  .get(
+    "http://cmd.jiskefet.io/api/logs?orderBy=logId&orderDirection=DESC",
+    header
+  )
+  .then(response => {
+    response.data.data.items.forEach(item => {
+      let newItem = {
+        logid: item.logId,
+        title: item.title,
+        subtype: item.subtype,
+        origin: item.origin,
+        creationTime: item.creationTime,
+        author: item.user.userId
+      };
+      newData.push(newItem);
+    });
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 function App() {
   const user = {
@@ -9,60 +89,10 @@ function App() {
     team: "Team 1",
     role: "Leader"
   };
-  const data = [
-    {
-      logid: 144,
-      title: "faucibus orci luctus",
-      subtype: "comment",
-      origin: "human",
-      creationTime: "22:24:20 30/07/2019",
-      author: 12
-    },
-    {
-      logid: 662,
-      title: "faucibus orci luctus",
-      subtype: "comment",
-      origin: "human",
-      creationTime: "22:24:20 30/07/2019",
-      author: 6
-    },
-    {
-      logid: 976,
-      title: "faucibus orci luctus",
-      subtype: "run",
-      origin: "process",
-      creationTime: "22:24:20 30/07/2019",
-      author: 1
-    },
-    {
-      logid: 976,
-      title: "faucibus orci luctus",
-      subtype: "run",
-      origin: "process",
-      creationTime: "22:24:20 30/07/2019",
-      author: 1
-    },
-    {
-      logid: 976,
-      title: "faucibus orci luctus",
-      subtype: "run",
-      origin: "process",
-      creationTime: "22:24:20 30/07/2019",
-      author: 1
-    },
-    {
-      logid: 976,
-      title: "faucibus orci luctus",
-      subtype: "run",
-      origin: "process",
-      creationTime: "22:24:20 30/07/2019",
-      author: 1
-    }
-  ];
   return (
     <div className="App">
       <Header />
-      <Main data={data} user={user} />
+      <Main data={fakedata} user={user} />
     </div>
   );
 }
