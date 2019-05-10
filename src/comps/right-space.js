63779a0b59fa6863d600857cf67f697344cec617
Steven Whitton <stevenawhitton@gmail.com>
2019-05-10 07:23:09 +0200

    Added working links

--- src/comps/right-space.js
+++ src/comps/right-space.js
@@ -17,22 +17,22 @@ function Rightspace() {
       <Shortcut
         color="important"
         text="View All Runs"
-        link="http://cmd.jiskefet.io/?orderBy=creationTime&orderDirection=DESC&pageSize=16&pageNumber=1"
+        link="http://cmd.jiskefet.io/runs?pageSize=16&pageNumber=1"
       />
       <Shortcut
         color="neutral"
         text="Filterset 1:Date creation 30/07/2019"
-        link="#"
+        link="http://cmd.jiskefet.io/logs?orderBy=creationTime&orderDirection=DESC&pageSize=16&pageNumber=1"
       />
       <Shortcut
         color="neutral"
         text="Filterset 2:All logs by Author 12"
-        link="#"
+        link="http://cmd.jiskefet.io/logs?orderBy=creationTime&orderDirection=DESC&pageSize=16&pageNumber=1"
       />
       <Shortcut
         color="neutral"
         text="Filterset 3:All logs by Team 1"
-        link="#"
+        link="http://cmd.jiskefet.io/logs?orderBy=creationTime&orderDirection=DESC&pageSize=16&pageNumber=1"
       />
     </div>
   );
